fa456a7eeef673548d76c385c7197b9082464af5
hutomosaleh <hutomo@windowslive.com>
2021-05-12 17:25:31 +0200

    Add channel listener

--- cogs/pomodoro.py
+++ cogs/pomodoro.py
@@ -10,5 +10,15 @@ class PomodoroCog(commands.Cog, name='Main Commands'):
     async def hello(self, ctx):
         await ctx.send("I'm alive")
 
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+
+        channel_id = [435126680722604036]
+        if after.channel:
+            print(f"Someone went inside channel {after.channel.id}")
+            if after.channel.id in channel_id:
+                await member.guild.system_channel.send(f"ID: {member.id} joined the VC on {after.channel.id}!")
+                #do whatever you want here
+
 def setup(bot):
     bot.add_cog(PomodoroCog(bot))
