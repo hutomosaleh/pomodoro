966afb11aff2c18a9f5820677dcafb1547b26912
hutomosaleh <hutomo@windowslive.com>
2021-05-15 18:46:52 +0200

    Differentiate between study time TODAY

--- cogs/pomodoro.py
+++ cogs/pomodoro.py
@@ -1,4 +1,5 @@
 import os
+import pytz
 from datetime import datetime
 from motor import motor_asyncio
 from discord.ext import commands
@@ -6,6 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('MONGODB_TOKEN')
+tz = pytz.timezone('Europe/Berlin')
 
 
 class PomodoroCog(commands.Cog, name='Main Commands'):
@@ -20,21 +22,29 @@ class PomodoroCog(commands.Cog, name='Main Commands'):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        channel_id = [435126680722604036]
+        channel_id = [435126680722604036, 780238348802981928, 780238348802981929]
+
         user = await self.db.find_one({"user_id": member.id})
         if user is None:
-            document = {"user_id": member.id, 'total_time': 0}
+            document = {"user_id": member.id, 'total_time': 0, 'total_time_today': 0, 'enter_time': 0}
             await self.db.insert_one(document)
-            await member.guild.system_channel.send(f"ID: {member.id} joined the VC on {after.channel.id}!")
+        
+        time_now = datetime.today()
         if after.channel:
             if after.channel.id in channel_id:
-                await self.db.update_one({"user_id": member.id}, {"$set": {'enter_time': datetime.today()}})
-                await member.guild.system_channel.send(f"ID: {member.id} joined the VC on {after.channel.id}!")
+                await self.db.update_one({"user_id": member.id}, {"$set": {'enter_time': time_now}})
+                if tz.localize(user['enter_time']).date() < tz.localize(time_now).date():
+                    await self.db.update_one({"user_id": member.id}, {"$set": {'total_time_today': 0}})
+                else:
+                    print("it's still the same day")
         else:
             if before.channel.id in channel_id:
-                delta_time = (datetime.today() - user['enter_time']).total_seconds() / 60
+                delta_time = round((time_now - user['enter_time']).total_seconds() / 60)
                 await self.db.update_one({"user_id": member.id}, {"$inc": {'total_time': delta_time}})
-                await member.guild.system_channel.send(f"You just studied for {delta_time} minutes!")
+                await self.db.update_one({"user_id": member.id}, {"$inc": {'total_time_today': delta_time}})
+                await self.bot.get_channel(746990302459592804).send("You just exited there")
+                if delta_time > 5:
+                    await member.guild.system_channel.send(f"You just studied for {delta_time} minutes!")
 
 def setup(bot):
     bot.add_cog(PomodoroCog(bot))
