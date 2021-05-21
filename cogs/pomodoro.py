fb5e754c78da33e66afe76f1a5f284b8ad65442a
hutomosaleh <hutomo@windowslive.com>
2021-05-21 17:04:52 +0200

    Add check_vc function but doesn't work with startup

--- cogs/pomodoro.py
+++ cogs/pomodoro.py
@@ -10,6 +10,9 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('MONGODB_TOKEN')
 tz = pytz.timezone('Europe/Berlin')
+channel_id = [435126680722604036, 780238348802981928, 780238348802981929]
+BOT_CHANNEL = 844625911411245117
+BOT_CHANNEL = 746990302459592804
 
 
 class PomodoroCog(commands.Cog, name='Main Commands'):
@@ -24,7 +27,6 @@ class PomodoroCog(commands.Cog, name='Main Commands'):
 
     @commands.command(aliases=['info'], help='Show study stats')
     async def stats(self, ctx):
-        update_info(ctx)
         user_id = ctx.message.author.id
         user = await self.db.find_one({"user_id": user_id})
         time_today = round(user['date_logged'][-1][1])
@@ -44,7 +46,7 @@ class PomodoroCog(commands.Cog, name='Main Commands'):
             return await ctx.send('Invalid argument')
         try:
             if duration < 0:
-                await ctx.send('Must be a real number, plese')
+                await ctx.send('Must be a positive number, please')
             elif duration > 300:
                 await ctx.send("Under 300 minutes, please")
             else:
@@ -57,41 +59,61 @@ class PomodoroCog(commands.Cog, name='Main Commands'):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        channel_id = [435126680722604036, 780238348802981928, 780238348802981929]
-
         user = await self.db.find_one({"user_id": member.id})
         time_now = datetime.today()
         date_now = time_now.strftime("%m/%d/%Y")
-
         if user is None:
             date_log = [[date_now, 0]]
             document = {"user_id": member.id, 'nick': member.name, 'total_time': 0, 'enter_time': time_now, 'studying': False, 'date_logged': date_log}
             await self.db.insert_one(document)
-        else:
-            new_day = tz.localize(user['enter_time']).date() < tz.localize(time_now).date()
-        
         if after.channel:
             if after.channel.id in channel_id:
-                await self.db.update_one({"user_id": member.id}, {"$set": {'enter_time': time_now}})
-                await self.db.update_one({"user_id": member.id}, {"$set": {'studying': True}})
-                if user and new_day:
-                        await self.db.update_one({"user_id": member.id}, {"$set": {'total_time_today': 0}})
-                        await self.db.update_one({"user_id": member.id}, {"$push": {'date_logged': [date_now, 0]}})
-                else:
-                    print("it's still the same day")
+                await self.start_study_session(member.id)
         else:
             if before.channel.id in channel_id:
                 user = await self.db.find_one({"user_id": member.id})
                 if user['studying'] is True:
-                    delta_time = round((time_now - user['enter_time']).total_seconds() / 60)
-                    new_time = user['date_logged'][-1]
-                    new_time[1] += delta_time
-                    if delta_time > 1:
-                        await self.db.update_one({"user_id": member.id}, {"$inc": {'total_time': delta_time}})
-                        await self.db.update_one({"user_id": member.id}, {"$pop": {'date_logged': 1}})
-                        await self.db.update_one({"user_id": member.id}, {"$push": {'date_logged': new_time}})
-                        await self.bot.get_channel(844625911411245117).send(f"{member.name} studied for {delta_time} minutes!")
-                    await self.db.update_one({"user_id": member.id}, {"$set": {'studying': False}})
+                    await self.finish_study_session(user)
+
+    async def check_vc(self, id):  # channel is ALWAYS empty when starting a bot
+        members = self.bot.get_channel(id).members
+        print(members)
+        for member in members:
+            user = await self.db.find_one({"user_id": member.id})
+            if user and not user['studying']:
+                print(f"{user['nick']} is studying!")
+                await self.start_study_session(user)
+    
+    async def start_study_session(self, id):
+        user = await self.db.find_one({"user_id": id})
+        time, date = get_time_date()
+        new_day = tz.localize(user['enter_time']).date() < tz.localize(time).date()
+
+        await self.db.update_one({"user_id": id}, {"$set": {'enter_time': time}})
+        await self.db.update_one({"user_id": id}, {"$set": {'studying': True}})
+
+        if user and new_day:
+                await self.db.update_one({"user_id": id}, {"$set": {'total_time_today': 0}})
+                await self.db.update_one({"user_id": id}, {"$push": {'date_logged': [date, 0]}})
+
+    async def finish_study_session(self, user):
+        time = get_time_date()[0]
+        delta_time = round((time - user['enter_time']).total_seconds() / 60)
+        new_time = user['date_logged'][-1]
+        new_time[1] += delta_time
+        if delta_time > 1:
+            await self.db.update_one({"user_id": user['user_id']}, {"$inc": {'total_time': delta_time}})
+            await self.db.update_one({"user_id": user['user_id']}, {"$pop": {'date_logged': 1}})
+            await self.db.update_one({"user_id": user['user_id']}, {"$push": {'date_logged': new_time}})
+            await self.bot.get_channel(BOT_CHANNEL).send(f"{user['nick']} studied for {delta_time} minutes!")
+        await self.db.update_one({"user_id": user['user_id']}, {"$set": {'studying': False}})
+
+
+def get_time_date():
+    time = datetime.today()
+    date = time.strftime("%m/%d/%Y")
+    return (time, date)
+
 
 def setup(bot):
     bot.add_cog(PomodoroCog(bot))
